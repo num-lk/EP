8d518f9777d6f2994b67bacb985dd01fbf27c8c3
Henrijs Strods <numlk@duck.com>
2024-05-04 02:42:28 +0300

    Window optimization

--- assets/nodes/player/player_controller.gd
+++ assets/nodes/player/player_controller.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 600.0
-const ACCEL = 2400.0
+const SPEED = 80.0
+const ACCEL = 1000.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
