bd331ac13cf25c36bbe736ca7e77c9e5e3b1f5f7
Henrijs Strods <numlk@duck.com>
2024-05-04 23:03:23 +0300

    Implemented vignette Implemented physics interpolation Improved tilemap layer sorting Removed politician splash screen

--- assets/nodes/player/player_controller.gd
+++ assets/nodes/player/player_controller.gd
@@ -1,13 +1,16 @@
 extends CharacterBody2D
 
-
 const SPEED = 80.0
 const ACCEL = 1000.0
 const JUMP_VELOCITY = -400.0
 
+var last_position;
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+# Get player sprite
+@onready var sprite = $Sprite2D;
 
 func _physics_process(delta):
 	
@@ -23,5 +26,12 @@ func _physics_process(delta):
 	# Set velocity
 	velocity = velocity.move_toward(dir.normalized() * SPEED, ACCEL * delta);
 	
+	# Record position before moving
+	last_position = position;
+	
 	# Move character
 	move_and_slide()
+	
+func _process(delta):
+	# Interpolate sprite
+	sprite.global_position = last_position.lerp(position, Engine.get_physics_interpolation_fraction());
