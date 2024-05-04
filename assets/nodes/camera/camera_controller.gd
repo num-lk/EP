e55355bf623729ba7e8422fb223754104c60086e
Henrijs Strods <numlk@duck.com>
2024-05-04 20:55:59 +0300

    Camera system improvements

--- assets/nodes/camera/camera_controller.gd
+++ assets/nodes/camera/camera_controller.gd
@@ -1,13 +1,33 @@
 extends Camera2D
 
-
-# Called when the node enters the scene tree for the first time.
+# Export variables
 @export var target: Node2D;
+@export var track_speed: float = 10;
+@export var offset_speed: float = 10;
+@export var offset_scale: float = 0.1;
 
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position = position.lerp(target.position, 0.2 * delta);
+	
+	# Account for delta time in camera tracking
+	var blend = pow(0.5, delta * track_speed)
+	
+	# Lerp to target location
+	position = target.position.lerp(position, blend);
+	
+	# Get current viewport
+	var vp = get_viewport();
+	
+	# Calculate mouse distance from center
+	var dist = vp.get_mouse_position() - Vector2(get_viewport_rect().size) / 2;
+	
+	# Scale distance
+	dist = dist * offset_scale;
+	
+	# Account for delta time in mouse tracking
+	blend = pow(0.5, delta * offset_speed)
+	
+	# Offset camerat toward mouse
+	offset = dist.lerp(offset, blend);
